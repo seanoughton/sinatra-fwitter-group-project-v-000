648a66e59c1c9db2acd8052bcb488806ef924a48
Sean Oughton <seanoughton@gmail.com>
2018-04-09 21:41:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,9 @@ class ApplicationController < Sinatra::Base
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
     else
-      binding.pry
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       @user.save
-      #session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect to '/tweets'
     end
   end
