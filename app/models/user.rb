2e509b4d7bd053ddb71b9ecfc9086baa868fa05f
Sean Oughton <seanoughton@gmail.com>
2018-04-10 17:02:21 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   def slug
-    self.username.downcase.split(" ").join("-")
+   username.downcase.gsub(" ","-")
   end
 
-  def self.find_by_slug(slug)
-   self.all.find{ |instance| instance.slug == slug }
-  end
+   def self.find_by_slug(slug)
+     User.all.find{|user| user.slug == slug}
+   end
 end
